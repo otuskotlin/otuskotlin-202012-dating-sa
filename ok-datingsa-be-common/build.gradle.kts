50e696a61059ee8a6e4cf5bbb654af2dc229dfb9
alexsumin <sumin.alexandr@gmail.com>
2021-02-24 22:39:06 +0300

    fixes after review

--- ok-datingsa-be-common/build.gradle.kts
+++ ok-datingsa-be-common/build.gradle.kts
@@ -6,11 +6,9 @@ group = rootProject.group
 version = rootProject.version
 
 dependencies {
-    val slf4jVersion: String by project
     val junitBomVersion: String by project
 
     implementation(kotlin("stdlib"))
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
 
     testImplementation(kotlin("test-junit5"))
     testImplementation(platform("org.junit:junit-bom:$junitBomVersion"))
